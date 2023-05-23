b70aba2bf318dce9f40944e1d4c9dab010153038
sathyagimenes <sathya.gimenes@gmail.com>
2023-05-22 23:28:26 -0300

    feat: initial files

---
+++ philo/includes/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-cama <sde-cama@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/22 23:07:49 by sde-cama          #+#    #+#             */
+/*   Updated: 2023/05/22 23:09:21 by sde-cama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>		// printf
+# include <stdlib.h>	// malloc, free
+# include <string.h>	// memset
+# include <unistd.h>	// write, usleep
+# include <pthread.h>	// pthread functions
+# include <sys/time.h>	// gettimeofday
+
+#endif
